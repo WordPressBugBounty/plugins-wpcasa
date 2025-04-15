95631ce1618a3d44a353c371184fa4c44fd9fc43
Sean Murphy <sean@iamseanmurphy.com>
2025-04-15 12:20:18 +0000

    1.4.0

--- wpcasa/includes/admin/views/option-colorpicker.php
+++ wpcasa/includes/admin/views/option-colorpicker.php
@@ -6,8 +6,6 @@
     $option_id = isset( $option['id'] ) ? $this->settings_name . '[' . $option['id'] . ']' : '';
     $option_css = sanitize_html_class( $this->settings_name . '_' . $option['id'] );
 
-    $option_options = isset( $option['options'] ) ? $option['options'] : '';
-
     $attributes = array();
     if ( isset( $option['attributes'] ) && is_array( $option['attributes'] ) ) {
       foreach ( $option['attributes'] as $attribute_name => $attribute_value ) {
@@ -16,14 +14,13 @@
     }
 
     $value = wpsight_get_option( $option['id'] );
-    if( !isset( $value ) && isset( $option['default'] ) ) $value = $option['default'];
+    if( empty( $value ) && isset( $option['default'] ) ) $value = $option['default'];
+
+    $placeholder = isset( $option['placeholder'] ) ? 'placeholder="' . $option['placeholder'] . '"'	: '';
 ?>
 
   <div class="wpsight-settings-field wpsight-settings-field-<?php echo esc_attr( $option_type ); ?>">
-    <select id="setting-<?php echo esc_attr( $option_css ); ?>" class="regular-text" name="<?php echo esc_attr( $option_id ); ?>" <?php echo esc_attr( implode( ' ', $attributes ) ); ?>><?php
-      foreach( $option_options as $key => $name )
-        echo '<option value="' . esc_attr( $key ) . '" ' . selected( $value, $key, false ) . '>' . esc_html( $name ) . '</option>'; ?>
-    </select>
+      <input type="text" name="<?php echo esc_attr( $option_id ); ?>" id="setting-<?php echo esc_attr( $option_css ); ?>" value="<?php echo esc_attr( $value ); ?>" class="wpsight-settings-colorpicker"/>
   </div>
 
 <?php } ?>
